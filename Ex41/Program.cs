f54f8b723ab901a4f7c7942f2f1b818ea3d63c31
Momihe <momihe@mail.ru>
2023-01-07 21:43:32 +0500

    HW6

---
+++ Ex41/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+void max(int[] array)
+{
+    int i = 0;
+    foreach (int element in array)
+    {
+        if (element > 0) i++;
+    }
+    Console.Write($"Количество чисел больше нуля равно: {i}");
+}
+
+Console.Clear();
+Console.Write("Введите элементы массива: ");
+int[] array = Console.ReadLine().Split(" ").Select(x => Convert.ToInt32(x)).ToArray();
+max(array);
